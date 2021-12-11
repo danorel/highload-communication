8d14774241da677bc88cc88719adeec80be41135
danorel <mail.ordan@gmail.com>
2021-12-11 14:09:41 +0200

    Add api-utils module

---
+++ libs/api-utils/README.md
@@ -0,0 +1,7 @@
+# `@communication/api-utils`
+
+TypeScript reusable api-utils for API communication
+
+## Release history
+
+All notable changes between releases of this tool are described in the [CHANGELOG.md](CHANGELOG.md) file.
---
+++ libs/api-utils/src/requests/requests.service.ts
@@ -0,0 +1,62 @@
+import { Injectable } from "@nestjs/common";
+import { HttpService } from "@nestjs/axios";
+import { Observable, map } from "rxjs";
+
+@Injectable()
+export class RequestsService {
+    constructor(private readonly httpService: HttpService) {}
+
+    get<ReturnType = unknown>(url): Observable<ReturnType> {
+        return this.httpService
+            .get<ReturnType>(
+                url,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .pipe(map((res) => res.data));
+    }
+
+    post<ReturnType = unknown, BodyType = any>(url: string, body: BodyType): Observable<ReturnType> {
+        return this.httpService
+            .post<ReturnType>(
+                url,
+                body,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .pipe(map((res) => res.data));
+    }
+
+    put<ReturnType = unknown, BodyType = any>(url: string, body: BodyType): Observable<ReturnType> {
+        return this.httpService
+            .put<ReturnType>(
+                url,
+                body,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .pipe(map((res) => res.data));
+    }
+
+    delete<ReturnType = unknown>(url): Observable<ReturnType> {
+        return this.httpService
+            .delete<ReturnType>(
+                url,
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            )
+            .pipe(map((res) => res.data));
+    }
+}
